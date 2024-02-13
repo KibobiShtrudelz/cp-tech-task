575eab408bfe54abbe38bb7f62dc0718d9fc3b55
KibobiShtrudelz <petar.georgiev.kolev@gmail.com>
2024-02-14 01:25:59 +0200

    remove unused packages

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,4 @@
 import { StrictMode } from 'react'
-import { Provider } from 'react-redux'
 import * as ReactDOM from 'react-dom/client'
 import { PrimeReactProvider } from 'primereact/api'
 import { BrowserRouter as Router } from 'react-router-dom'
@@ -7,8 +6,6 @@ import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
 import { App } from './app/app'
-
-import { store } from './redux/store'
 
 import 'primereact/resources/themes/saga-blue/theme.css'
 
@@ -26,16 +23,14 @@ const primeReactValues = {
 
 root.render(
   <StrictMode>
-    <Provider store={store}>
-      <QueryClientProvider client={queryClient}>
-        <Router>
-          <PrimeReactProvider value={primeReactValues}>
-            <App />
-          </PrimeReactProvider>
-        </Router>
+    <QueryClientProvider client={queryClient}>
+      <Router>
+        <PrimeReactProvider value={primeReactValues}>
+          <App />
+        </PrimeReactProvider>
+      </Router>
 
-        <ReactQueryDevtools initialIsOpen={false} />
-      </QueryClientProvider>
-    </Provider>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   </StrictMode>
 )
--- src/redux/store-hooks.ts
+++
@@ -1,9 +0,0 @@
-import { useDispatch, useSelector, TypedUseSelectorHook } from 'react-redux'
-
-import { store } from './store'
-
-export type RootState = ReturnType<typeof store.getState>
-export type AppDispatch = typeof store.dispatch
-
-export const useAppDispatch = () => useDispatch<AppDispatch>()
-export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector
