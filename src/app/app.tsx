050e69683474166750b62daa37620102491abfe3
KibobiShtrudelz <petar.georgiev.kolev@gmail.com>
2024-02-14 02:11:55 +0200

    wip: save stye changes in favor of url filter logic

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -79,216 +79,218 @@ export function App() {
         <Divider />
       </section>
 
-      <section className={styles.filterSection}>
-        <h1>Virtual Scroll Filters</h1>
+      <h1 className={styles.virtualScrollTitle}>Virtual Scroll Filters</h1>
 
-        <form onSubmit={onSubmit}>
-          <Controller
-            name="timestampFrom"
-            control={control}
-            rules={{ required: 'Requis' }}
-            render={({ field, fieldState }) => (
-              <div>
-                <span className="p-float-label">
-                  <Dropdown
-                    showClear
-                    id={field.name}
-                    name={field.name}
-                    inputId={field.name}
-                    value={field.value}
-                    options={getDays(31)}
-                    focusInputRef={field.ref}
-                    className={clsx(styles.dropdown, fieldState.error && 'p-invalid')}
-                    onChange={e => field.onChange(e.value)}
-                  />
+      <div className={styles.filtersScrollWrapper}>
+        <section className={styles.filterSection}>
+          <form onSubmit={onSubmit}>
+            <Controller
+              name="timestampFrom"
+              control={control}
+              rules={{ required: 'Requis' }}
+              render={({ field, fieldState }) => (
+                <div>
+                  <span className="p-float-label">
+                    <Dropdown
+                      showClear
+                      id={field.name}
+                      name={field.name}
+                      inputId={field.name}
+                      value={field.value}
+                      options={getDays(31)}
+                      focusInputRef={field.ref}
+                      className={clsx(styles.dropdown, fieldState.error && 'p-invalid')}
+                      onChange={e => field.onChange(e.value)}
+                    />
 
-                  <label htmlFor={field.name}>Timestamp From day</label>
-                </span>
+                    <label htmlFor={field.name}>Timestamp From day</label>
+                  </span>
 
-                {getFormErrorMessage('timestampFrom')}
-              </div>
-            )}
-          />
-
-          <Controller
-            name="timestampTo"
-            control={control}
-            rules={{ required: 'Requis' }}
-            render={({ field, fieldState }) => (
-              <div>
-                {' '}
-                <span className="p-float-label">
-                  <Dropdown
-                    showClear
-                    id={field.name}
-                    name={field.name}
-                    inputId={field.name}
-                    value={field.value}
-                    options={getDays(31)}
-                    focusInputRef={field.ref}
-                    className={clsx(styles.dropdown, fieldState.error && 'p-invalid')}
-                    onChange={e => field.onChange(e.value)}
-                  />
-
-                  <label htmlFor={field.name}>Timestamp To day</label>
-                </span>
-                {getFormErrorMessage('timestampTo')}
-              </div>
-            )}
-          />
-
-          <Controller
-            name="status"
-            control={control}
-            render={({ field, fieldState }) => (
-              <div>
-                <span className="p-float-label">
-                  <Dropdown
-                    showClear
-                    id={field.name}
-                    name={field.name}
-                    inputId={field.name}
-                    optionLabel="name"
-                    value={field.value}
-                    options={statusTypes}
-                    focusInputRef={field.ref}
-                    className={clsx(styles.dropdown, fieldState.error && 'p-invalid')}
-                    onChange={e => field.onChange(e.value)}
-                  />
-
-                  <label htmlFor={field.name}>Status</label>
-                </span>
-
-                {getFormErrorMessage('status')}
-              </div>
-            )}
-          />
-
-          <Controller
-            name="issueType"
-            control={control}
-            render={({ field, fieldState }) => (
-              <div>
-                <span className="p-float-label">
-                  <Dropdown
-                    showClear
-                    id={field.name}
-                    name={field.name}
-                    inputId={field.name}
-                    optionLabel="name"
-                    value={field.value}
-                    options={issueTypes}
-                    focusInputRef={field.ref}
-                    className={clsx(styles.dropdown, fieldState.error && 'p-invalid')}
-                    onChange={e => field.onChange(e.value)}
-                  />
-
-                  <label htmlFor={field.name}>Issue Type</label>
-                </span>
-
-                {getFormErrorMessage('issueType')}
-              </div>
-            )}
-          />
-
-          <Controller
-            name="url"
-            control={control}
-            render={({ field, fieldState }) => (
-              <div>
-                <span className="p-float-label">
-                  <InputText
-                    id={field.name}
-                    name={field.name}
-                    value={field.value || ''}
-                    className={clsx(fieldState.error && 'p-invalid')}
-                    onChange={e => field.onChange(e.target.value)}
-                  />
-
-                  <label htmlFor={field.name}>URL</label>
-                </span>
-
-                {getFormErrorMessage('url')}
-              </div>
-            )}
-          />
-
-          <Controller
-            name="responseTimeFrom"
-            control={control}
-            render={({ field, fieldState }) => (
-              <div>
-                <span className="p-float-label">
-                  <InputText
-                    keyfilter="int"
-                    id={field.name}
-                    name={field.name}
-                    value={field.value || ''}
-                    className={clsx(fieldState.error && 'p-invalid')}
-                    onChange={e => field.onChange(e.target.value)}
-                  />
-
-                  <label htmlFor={field.name}>Response time From</label>
-                </span>
-
-                {getFormErrorMessage('responseTimeFrom')}
-              </div>
-            )}
-          />
-
-          <Controller
-            name="responseTimeTo"
-            control={control}
-            render={({ field, fieldState }) => (
-              <div>
-                <span className="p-float-label">
-                  <InputText
-                    keyfilter="int"
-                    id={field.name}
-                    name={field.name}
-                    value={field.value || ''}
-                    className={clsx(fieldState.error && 'p-invalid')}
-                    onChange={e => field.onChange(e.target.value)}
-                  />
-
-                  <label htmlFor={field.name}>Response time To</label>
-                </span>
-
-                {getFormErrorMessage('responseTimeTo')}
-              </div>
-            )}
-          />
-
-          <Button
-            type="reset"
-            label="Reset"
-            severity="secondary"
-            className={styles.button}
-            onClick={reset as React.MouseEventHandler<HTMLButtonElement>}
-          />
-
-          <Button className={styles.button} type="submit" label="Submit" />
-        </form>
-      </section>
-
-      <section className={styles.virtualScrollSection}>
-        <div className={styles.container}>
-          {filteredAccessLogs ? (
-            <VirtualScroller
-              showLoader
-              delay={250}
-              itemSize={230}
-              items={filteredAccessLogs}
-              style={{ height: '500px' }}
-              loadingTemplate={getLoadingTemplate}
-              itemTemplate={getVirtualScrollerItemTemplate}
-              className="border-1 surface-border border-round"
+                  {getFormErrorMessage('timestampFrom')}
+                </div>
+              )}
             />
-          ) : (
-            <h3>Oups, aucune entrée avec de tels critères :)</h3>
-          )}
-        </div>
-      </section>
+
+            <Controller
+              name="timestampTo"
+              control={control}
+              rules={{ required: 'Requis' }}
+              render={({ field, fieldState }) => (
+                <div>
+                  {' '}
+                  <span className="p-float-label">
+                    <Dropdown
+                      showClear
+                      id={field.name}
+                      name={field.name}
+                      inputId={field.name}
+                      value={field.value}
+                      options={getDays(31)}
+                      focusInputRef={field.ref}
+                      className={clsx(styles.dropdown, fieldState.error && 'p-invalid')}
+                      onChange={e => field.onChange(e.value)}
+                    />
+
+                    <label htmlFor={field.name}>Timestamp To day</label>
+                  </span>
+                  {getFormErrorMessage('timestampTo')}
+                </div>
+              )}
+            />
+
+            <Controller
+              name="status"
+              control={control}
+              render={({ field, fieldState }) => (
+                <div>
+                  <span className="p-float-label">
+                    <Dropdown
+                      showClear
+                      id={field.name}
+                      name={field.name}
+                      inputId={field.name}
+                      optionLabel="name"
+                      value={field.value}
+                      options={statusTypes}
+                      focusInputRef={field.ref}
+                      className={clsx(styles.dropdown, fieldState.error && 'p-invalid')}
+                      onChange={e => field.onChange(e.value)}
+                    />
+
+                    <label htmlFor={field.name}>Status</label>
+                  </span>
+
+                  {getFormErrorMessage('status')}
+                </div>
+              )}
+            />
+
+            <Controller
+              name="issueType"
+              control={control}
+              render={({ field, fieldState }) => (
+                <div>
+                  <span className="p-float-label">
+                    <Dropdown
+                      showClear
+                      id={field.name}
+                      name={field.name}
+                      inputId={field.name}
+                      optionLabel="name"
+                      value={field.value}
+                      options={issueTypes}
+                      focusInputRef={field.ref}
+                      className={clsx(styles.dropdown, fieldState.error && 'p-invalid')}
+                      onChange={e => field.onChange(e.value)}
+                    />
+
+                    <label htmlFor={field.name}>Issue Type</label>
+                  </span>
+
+                  {getFormErrorMessage('issueType')}
+                </div>
+              )}
+            />
+
+            <Controller
+              name="url"
+              control={control}
+              render={({ field, fieldState }) => (
+                <div>
+                  <span className="p-float-label">
+                    <InputText
+                      id={field.name}
+                      name={field.name}
+                      value={field.value || ''}
+                      className={clsx(fieldState.error && 'p-invalid')}
+                      onChange={e => field.onChange(e.target.value)}
+                    />
+
+                    <label htmlFor={field.name}>URL</label>
+                  </span>
+
+                  {getFormErrorMessage('url')}
+                </div>
+              )}
+            />
+
+            <Controller
+              name="responseTimeFrom"
+              control={control}
+              render={({ field, fieldState }) => (
+                <div>
+                  <span className="p-float-label">
+                    <InputText
+                      keyfilter="int"
+                      id={field.name}
+                      name={field.name}
+                      value={field.value || ''}
+                      className={clsx(fieldState.error && 'p-invalid')}
+                      onChange={e => field.onChange(e.target.value)}
+                    />
+
+                    <label htmlFor={field.name}>Response time From</label>
+                  </span>
+
+                  {getFormErrorMessage('responseTimeFrom')}
+                </div>
+              )}
+            />
+
+            <Controller
+              name="responseTimeTo"
+              control={control}
+              render={({ field, fieldState }) => (
+                <div>
+                  <span className="p-float-label">
+                    <InputText
+                      keyfilter="int"
+                      id={field.name}
+                      name={field.name}
+                      value={field.value || ''}
+                      className={clsx(fieldState.error && 'p-invalid')}
+                      onChange={e => field.onChange(e.target.value)}
+                    />
+
+                    <label htmlFor={field.name}>Response time To</label>
+                  </span>
+
+                  {getFormErrorMessage('responseTimeTo')}
+                </div>
+              )}
+            />
+
+            <Button
+              type="reset"
+              label="Reset"
+              severity="secondary"
+              className={styles.button}
+              onClick={reset as React.MouseEventHandler<HTMLButtonElement>}
+            />
+
+            <Button className={styles.button} type="submit" label="Submit" />
+          </form>
+        </section>
+
+        <section className={styles.virtualScrollSection}>
+          <div className={styles.container}>
+            {filteredAccessLogs ? (
+              <VirtualScroller
+                showLoader
+                delay={250}
+                itemSize={230}
+                items={filteredAccessLogs}
+                style={{ height: '560px', minWidth: '320px' }}
+                loadingTemplate={getLoadingTemplate}
+                itemTemplate={getVirtualScrollerItemTemplate}
+                className="border-1 surface-border border-round"
+              />
+            ) : (
+              <h3>Oups, aucune entrée avec de tels critères :)</h3>
+            )}
+          </div>
+        </section>
+      </div>
     </div>
   )
 }
